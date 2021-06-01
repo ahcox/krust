381e84097f80720b968e4acbcd6de4aefaf6f22b
Andrew Cox <git@ahcox.com>
2021-06-01 19:40:41 +0100

    Validation errors on image view swizzles for swapchain fixed.

--- krust/public-api/vulkan-utils.cpp
+++ krust/public-api/vulkan-utils.cpp
@@ -55,7 +55,7 @@ VkImageCreateInfo CreateDepthImageInfo(const uint32_t presentQueueFamily, const 
     info.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
     info.queueFamilyIndexCount = 1,
     info.pQueueFamilyIndices = &presentQueueFamily,
-    info.initialLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+    info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; // But I want to get into: VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 
   return info;
 }
@@ -67,10 +67,10 @@ VkImageView CreateDepthImageView(VkDevice device, VkImage image, const VkFormat 
     viewInfo.image = image,
     viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D,
     viewInfo.format = format,
-    viewInfo.components.r = VK_COMPONENT_SWIZZLE_ZERO,
-    viewInfo.components.g = VK_COMPONENT_SWIZZLE_ZERO,
-    viewInfo.components.b = VK_COMPONENT_SWIZZLE_ZERO,
-    viewInfo.components.a = VK_COMPONENT_SWIZZLE_ZERO,
+    viewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY,
+    viewInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY,
+    viewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY,
+    viewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY,
     viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,
     viewInfo.subresourceRange.baseMipLevel = 0,
     viewInfo.subresourceRange.levelCount = 1,
