7868aec7e55f9538bc9edcd90d0af782783b2748
Andrew Cox <git@ahcox.com>
2021-09-07 16:29:20 +0100

    Solid sphere option. Backfaces of shell spheres correct normal. Max bounces reduced based on 8 bit final precision.

--- krust-examples/rt1/intersections.inc.glsl
+++ krust-examples/rt1/intersections.inc.glsl
@@ -67,7 +67,7 @@ float isect_sphere2(const vec3 ray_origin, const vec3 ray_dir_unit, const float 
 
 /// Return first hit of ray with sphere surface if outside the sphere.
 ///
-/// @note Misses back faces when the camera is inside the sphere:
+/// @note Misses back faces when the ray starts inside the sphere:
 /// Followed Listing 13 in RTIOW, Section 6.2 to generate this:
 float isect_sphere(const vec3 ray_origin, const vec3 ray_dir, const float t_min, const vec3 sphere_centre, const float sphere_radius)
 {
@@ -88,7 +88,7 @@ float isect_sphere(const vec3 ray_origin, const vec3 ray_dir, const float t_min,
 }
 
 /// Return first hit of ray with sphere surface if outside the sphere.
-bool sphere_hit(
+bool sphere_hit_outside(
     in const vec3 ray_origin,
     in const vec3 ray_dir_unit, // We rely on this being unit length to save work, including a divide, per sphere.
     in const vec4 sphere,
@@ -107,6 +107,63 @@ bool sphere_hit(
         const float e = sqrt(discriminator);
         const float root1 = (-half_b-e);
         t = root1;
+        hit = true;
+    }
+    return hit;
+}
+
+/// Return first hit with sphere surface.
+bool sphere_hit(
+    in const vec3 ray_origin,
+    in const vec3 ray_dir_unit, // We rely on this being unit length to save work, including a divide, per sphere.
+    in const vec4 sphere,
+    out float t)
+{
+    const vec3 sphere_centre = sphere.xyz;
+    const float sphere_radius = sphere.w;
+
+    const vec3 sphere_to_ray = ray_origin - sphere_centre;
+    const float half_b = dot(sphere_to_ray, ray_dir_unit);
+    const float c = length_squared(sphere_to_ray) - sphere_radius * sphere_radius;
+    const float discriminator = half_b * half_b - c;
+
+    bool hit = false;
+    if(discriminator >= 0.0f){
+        const float e = sqrt(discriminator);
+        const float root1 = (-half_b-e);
+        const float root2 = (-half_b+e);
+        t = root1;
+        if(root1 <= 0.0f){
+            t = root2;
+        }
+        hit = true;
+    }
+    return hit;
+}
+
+/// Output both hits of a sphere:
+bool sphere_hits(
+    in const vec3 ray_origin,
+    in const vec3 ray_dir_unit, // We rely on this being unit length to save work, including a divide, per sphere.
+    in const vec4 sphere,
+    out float t1,
+    out float t2)
+{
+    const vec3 sphere_centre = sphere.xyz;
+    const float sphere_radius = sphere.w;
+
+    const vec3 sphere_to_ray = ray_origin - sphere_centre;
+    const float half_b = dot(sphere_to_ray, ray_dir_unit);
+    const float c = length_squared(sphere_to_ray) - sphere_radius * sphere_radius;
+    const float discriminator = half_b * half_b - c;
+
+    bool hit = false;
+    if(discriminator >= 0.0f){
+        const float e = sqrt(discriminator);
+        const float root1 = (-half_b-e);
+        const float root2 = (-half_b+e);
+        t1 = root1;
+        t2 = root2;
         hit = true;
     }
     return hit;
--- krust-examples/rt1/rtow_diffuse_grey.comp.glsl
+++ krust-examples/rt1/rtow_diffuse_grey.comp.glsl
@@ -5,6 +5,11 @@
 #include "header.inc.glsl"
 #include "intersections.inc.glsl"
 
+// Are spheres solids or just infinitely thin surface shells?
+#ifndef SPHERES_ARE_SOLID
+#define SPHERES_ARE_SOLID 0
+#endif // SPHERES_ARE_SOLID
+
 // Define the number of samples of each pixel in X and Y by passing a value for this to the
 // GLSL compiler (AA = 4 -> 16 samples, AA = 10 -> 100 samples):
 #ifndef AA
@@ -80,14 +85,48 @@ struct HitRecord {
 /// @return true if there was a hit, else false.
 bool closest_sphere_hit(in vec3 ray_origin, in vec3 ray_dir_unit, in float t_min, inout HitRecord hit)
 {
+    bool found_hit = false;
+    for(uint i = 0; i < spheres.length(); ++i)
+    {
+        //float t;
+        //if(sphere_hit(ray_origin, ray_dir_unit, spheres[i], t)){
+        float t1, t2;
+        if(sphere_hits(ray_origin, ray_dir_unit, spheres[i], t1, t2)){
+            if(t1 >= t_min && t1 < hit.t){
+                hit.t = t1;
+                hit.prim = uint16_t(i);
+                found_hit = true;
+                hit.front_face = true;
+            }
+            if(t2 >= t_min && t2 < hit.t){
+                hit.t = t2;
+                hit.prim = uint16_t(i);
+                found_hit = true;
+                hit.front_face = false;
+            }
+
+        }
+    }
+    return found_hit;
+}
+
+bool closest_solid_sphere_hit(in vec3 ray_origin, in vec3 ray_dir_unit, in float t_min, inout HitRecord hit)
+{
     hit.front_face = true; // Our sphere intersect fails on backfaces currently.
     bool found_hit = false;
     for(uint i = 0; i < spheres.length(); ++i)
     {
-        float t;
-        if(sphere_hit(ray_origin, ray_dir_unit, spheres[i], t)){
-            if(t >= t_min && t < hit.t){
-                hit.t = t;
+        float t1, t2;
+        if(sphere_hits(ray_origin, ray_dir_unit, spheres[i], t1, t2)){
+            if(t1 < t_min && t2 > t_min){
+                hit.t = t_min;
+                hit.prim = uint16_t(i);
+                found_hit = true;
+                hit.front_face = false;
+                break;
+            }
+            if(t1 >= t_min && t1 < hit.t){
+                hit.t = t1;
                 hit.prim = uint16_t(i);
                 found_hit = true;
             }
@@ -96,22 +135,26 @@ bool closest_sphere_hit(in vec3 ray_origin, in vec3 ray_dir_unit, in float t_min
     return found_hit;
 }
 
-const uint MAX_REC = 20u;
+const uint MAX_BOUNCE = 9u;
 /// Equivalent to `ray_color()`in section 8.2 of Ray Tracing in One Week
 /// but with recursion replaced by a loop.
-vec3 shoot_ray_recursive(inout highp uint32_t seed, in vec3 ray_origin, in vec3 ray_dir_unit)
+vec3 shoot_ray(inout highp uint32_t seed, in vec3 ray_origin, in vec3 ray_dir_unit)
 {
     float attenuation = 1.0f;
-    for(uint ray_segment = 0; ray_segment < MAX_REC; ++ray_segment)
+    for(uint ray_segment = 0; ray_segment < MAX_BOUNCE; ++ray_segment)
     {
         HitRecord hit;
         hit.t = MISS;
-        if(closest_sphere_hit(ray_origin, ray_dir_unit, 0.001f, hit)){
 
+#if SPHERES_ARE_SOLID
+        if(closest_solid_sphere_hit(ray_origin, ray_dir_unit, 0.001f, hit)){
+#else
+        if(closest_sphere_hit(ray_origin, ray_dir_unit, 0.001f, hit)){
+#endif
             // Bounce a ray off in a random direction:
             vec4 sphere = spheres[uint(hit.prim)];
             const vec3 new_ray_origin = ray_origin + ray_dir_unit * hit.t;
-            const vec3 surface_norm = (new_ray_origin - sphere.xyz) * (1.0f / sphere.w);
+            const vec3 surface_norm = (new_ray_origin - sphere.xyz) * (1.0f / sphere.w) * (hit.front_face ? 1.0f : -1.0f);
             ray_dir_unit = normalize((new_ray_origin + surface_norm + rand_point_in_unit_sphere(seed)) - new_ray_origin);
             ray_origin = new_ray_origin;
             attenuation *= 0.5f;
@@ -121,8 +164,8 @@ vec3 shoot_ray_recursive(inout highp uint32_t seed, in vec3 ray_origin, in vec3 
     }
 
     // Default to background colour:
-    // To match the CPU version we'd branch on having hit MAX_REC and shade black
-    // instead of sky colour but attenuation would be 1/(2^MAC_REC) in that case anyway.
+    // To match the CPU version we'd branch on having hit MAX_BOUNCE and shade black
+    // instead of sky colour but attenuation would be 1/(2^MAX_BOUNCE) in that case anyway.
     const float wg_shade = (ray_dir_unit.y + 1.0f) * 0.5f;
     vec3 pixel = mix(vec3(1,1,1), vec3(0.5f,0.7f,1.0f), wg_shade);
     pixel *= attenuation;
@@ -195,7 +238,7 @@ void main()
     const vec3 ray_dir_raw = (fp.ray_target_origin + fp.ray_target_right * (screen_coord_x + 0.5f) + fp.ray_target_up * (screen_coord_y + 0.5f)) - fp.ray_origin;
     const vec3 ray_dir_unit = normalize(ray_dir_raw);
 
-    vec3 pixel = shoot_ray_recursive(random_seed, fp.ray_origin, ray_dir_unit);
+    vec3 pixel = shoot_ray(random_seed, fp.ray_origin, ray_dir_unit);
 #else
     // Shoot a grid of rays across the pixel:
     vec3 pixel = {0, 0, 0};
@@ -210,7 +253,7 @@ void main()
             const vec3 ray_dir_raw = (fp.ray_target_origin + fp.ray_target_right * sample_x + fp.ray_target_up * sample_y) - fp.ray_origin;
             const vec3 ray_dir_unit = vec3(normalize(ray_dir_raw));
 
-            pixel += shoot_ray_recursive(random_seed, fp.ray_origin, ray_dir_unit);
+            pixel += shoot_ray(random_seed, fp.ray_origin, ray_dir_unit);
         }
     }
     pixel *= INV_NUM_SAMPLES;
