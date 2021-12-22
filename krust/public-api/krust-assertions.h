b68e4b5039ce08066ff1a7462b3bf2571f1a9737
Andrew Cox <git@ahcox.com>
2021-12-22 17:35:35 +0000

    File-level doxygen comment on assertions header.

--- krust/public-api/krust-assertions.h
+++ krust/public-api/krust-assertions.h
@@ -1,18 +1,18 @@
 #ifndef KRUST_COMMON_PUBLIC_API_KRUST_ASSERTIONS_H_
 #define KRUST_COMMON_PUBLIC_API_KRUST_ASSERTIONS_H_
 
-// Copyright (c) 2016 Andrew Helge Cox
-// 
+// Copyright (c) 2016-2021 Andrew Helge Cox
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +24,11 @@
 
 // External includes:
 #include <cassert>
+
+/**
+ * @file Wrappers for compile time and run time assertions allowing enabling at
+ * multiple levels of performance impact, trading against thoroughness.
+ */
 
 /**
  * @brief Assertion of a compile-time expression.
