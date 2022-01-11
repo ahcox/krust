cb43c26cb6e8854294ca243d3e8825161dfeb49d
Andrew Cox <git@ahcox.com>
2022-01-11 18:24:47 +0000

    Noted need to split file. [trivia]

--- krust/public-api/vulkan-utils.h
+++ krust/public-api/vulkan-utils.h
@@ -2,17 +2,17 @@
 #define KRUST_COMMON_PUBLIC_API_VULKAN_UTILS_H
 
 // Copyright (c) 2016-2021 Andrew Helge Cox
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,6 +23,8 @@
 
 /**
  * @file Helpers and utilities for the Vulkan API.
+ *
+ * @todo Separate out helpers that use vulkan-objects.h ownership wrappers into their own file.
  */
 
 // Internal includes:
